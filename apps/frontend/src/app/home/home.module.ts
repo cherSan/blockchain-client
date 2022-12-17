efd5abc0222fd7d93c2064092130c8db2ae61dac
cherSan <chernushevich.alexander@gmail.com>
2022-12-17 18:52:07 -0500

    initialize home routing (#5)

--- apps/frontend/src/app/home/home.module.ts
+++ apps/frontend/src/app/home/home.module.ts
@@ -1,22 +1,60 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPangeComponent } from './main-pange/main-pange.component';
 import { RouterModule, Routes } from "@angular/router";
+import { LayoutsModule } from "@blockchain_client/ui/layouts";
+
+import { MainPangeComponent } from './main-pange/main-pange.component';
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzDescriptionsModule } from "ng-zorro-antd/descriptions";
+import { NzStatisticModule } from "ng-zorro-antd/statistic";
+import { NzPageHeaderModule } from "ng-zorro-antd/page-header";
+import { NzTabsModule } from "ng-zorro-antd/tabs";
+import { NzSpaceModule } from "ng-zorro-antd/space";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { StatisticPageComponent } from './statistic-page/statistic-page.component';
+import { InfoPageComponent } from './info-page/info-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainPangeComponent
+    component: MainPangeComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: "full",
+        redirectTo: 'statistic'
+      },
+      {
+        path: 'statistic',
+        component: StatisticPageComponent
+      },
+      {
+        path: 'info',
+        component: InfoPageComponent
+      }
+    ]
   }
 ]
 
 @NgModule({
   declarations: [
-    MainPangeComponent
+    MainPangeComponent,
+    StatisticPageComponent,
+    InfoPageComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    LayoutsModule,
+    NzMenuModule,
+    NzButtonModule,
+    NzDescriptionsModule,
+    NzStatisticModule,
+    NzPageHeaderModule,
+    NzTabsModule,
+    NzSpaceModule,
+    NzInputModule
   ]
 })
 export class HomeModule { }
--- apps/frontend/src/app/home/main-pange/main-pange.component.ts
+++ apps/frontend/src/app/home/main-pange/main-pange.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'main-pange',
   templateUrl: './main-pange.component.html',
-  styleUrls: ['./main-pange.component.css']
+  styleUrls: ['./main-pange.component.scss']
 })
 export class MainPangeComponent implements OnInit {
 
