575a3e8916e1b44679a6e14fce36dfe2b0031143
cherSan <chernushevich.alexander@gmail.com>
2022-12-16 21:05:41 -0500

    Feature/add auto generator (#2)
    * add auto generator
    
    * add auto generator and link FE with SS

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { RouterModule } from '@angular/router';
 import { NzDropDownModule } from "ng-zorro-antd/dropdown";
 import { NzIconModule } from "ng-zorro-antd/icon";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { GraphQlClientModule } from "@untitled3/graph-ql-client";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     NoopAnimationsModule,
     RouterModule.forRoot([], { initialNavigation: "enabledBlocking" }),
+    GraphQlClientModule,
     NzDropDownModule,
-    NzIconModule
+    NzIconModule,
+    NzInputModule,
+    NzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
