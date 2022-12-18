514556e6e5254b4642ca258f30cb9d37765ee055
cherSan <chernushevich.alexander@gmail.com>
2022-12-17 19:40:58 -0500

    Development (#8)
    * add server side based on GraphQL and ORM (#1)
    
    * Feature/add auto generator (#2)
    
    * add auto generator
    
    * add auto generator and link FE with SS
    
    * --error.log
    
    * refactor project name
    
    * add ui library
    
    * implement initialization guard and authorization (#3)
    
    * initialize home routing (#5)
    
    * Feature/home page initilize (#7)
    
    * initialize home routing
    
    * initialize home routing

--- libs/ui/layouts/src/lib/layouts.module.ts
+++ libs/ui/layouts/src/lib/layouts.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzLayoutModule
+  ],
+  declarations: [
+    MainLayoutComponent
+  ],
+  exports: [
+    MainLayoutComponent
+  ]
 })
 export class LayoutsModule {}
